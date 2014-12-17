5c93085efe31d28ef25fb0258dacdc760a4a4fdf
edave64 <edave64@googlemail.com>
2014-12-17 12:11:34 +0100

    - Commit before lambda refactor

--- scripts/protojazz-min.js
+++ scripts/protojazz-min.js
@@ -1,18 +1,4 @@
-/* Protojazz 0.5
- * (c) 2011 EDave
- *
- * Protojazz is freely distributable under the terms of an MIT-style license.
- * For details, see the Protojazz web site: http://edave64.github.com/protojazz/
-*/
-if(!$)var $={};$.proto=function(b,a){if(!a)a=b,b=$.global;var c=function(){(this
-.init||function(){}).apply(this,arguments)};c.prototype={};if(typeof a==
-"function"){var d={};a.call(d);a=d}b.inherited&&b.inherited(a);a.self&&($.
-extend(c,a.self),delete a.self);c.prototype=new b;$.extendP(c,a);c.prototype.
-_parent=b.prototype;return c};$.applyAttr=function(b,a){if(a.getters)for(var c
-in a.getters)b.__defineGetter__(c,a.getters[c]);if(a.setters)for(var d in a.
-setters)b.__defineSetter__(d,a.setters[d]);delete a.getters};$.extend=function(b
-,a){if(typeof a=="function")a.call(b);else for(var c in a)a.hasOwnProperty(c)&&(
-b[c]=a[c]);$.applyAttr(a,a)};$.extendP=function(b,a){$.extend(b.prototype,a)};$.
-global=$.proto(function(){},{parent:function(b){if(typeof this._parent[b]==
-"function")return this._parent[b].apply(this,[].slice.call(arguments,1));return
-this._parent[b]}});if(typeof module!='undefined')module.exports=$
+// Protojazz 0.8 (c) 2011 EDave
+// Protojazz is freely distributable under the terms of an MIT-style license.
+// For details, see the Protojazz web site: http://edave64.github.com/protojazz/
+(function($){var z='function';$.protojazzVersion="0.8";$.extend=function(a,b){if(typeof b==z)b.call(a);else for(var c in b)b.hasOwnProperty(c)&&(a[c]=b[c]);return a};$.getP=Object.getPrototypeOf||function(a){return a.__proto__};$.proto=function(d,b){var c=$.construct();if(!b)b=d,d=$.global;var e=c.prototype=new d($.noInit);e.constructor=c;$.extend(e,b);e.self&&$.extend(c,e.self);delete e.self;d.inherited&&d.inherited(c);return c};$.noInit={};$.construct=function(){return function b(){var c=arguments,e=c[0]!=$.noInit;if(!(this instanceof b)){var f=new b($.noInit);e&&f.init.apply(f,c);return f}e&&this.init.apply(this,c)}};$.global=$.proto(function(){},{Super:function(d){var b=$.getP($.getP(this));if(!d)return b;b=b[d];return typeof b==z?b.apply(this,[].slice.call(arguments,1)):b},init:function(){},isA:function(a){return this instanceof a}})})(this._||(this._={}));
