5b8e72de7b4d04f77155e67d7650ea3228bdfdb1
David Bachmann <db@iap.de>
2017-10-09 09:54:03 +0200

    let instead of var

--- src/polcalib.d.ts
+++ src/polcalib.d.ts
@@ -2,9 +2,9 @@
  * Created by edave on 07.10.16.
  */
 declare module polcaLib {
-    export var get;
-    export var set;
-    export var pow;
-    export var number;
-    export var compare;
+    export let get;
+    export let set;
+    export let pow;
+    export let number;
+    export let compare;
 }
