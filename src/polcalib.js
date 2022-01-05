765c811f06ace9b04b8c522e2a79052f0d326b44
sofia <12581975+opensofias@users.noreply.github.com>
2022-01-05 19:21:30 +0100

    correct error messages for shift and unshift

--- src/polcalib.js
+++ src/polcalib.js
@@ -186,12 +186,12 @@ polcaLib = (function () {
         },
         'unshift >|' (value, substack) {
             if (!(substack instanceof Polca.SubStack))
-                throw new Error ("push is not implemented for this type");
+                throw new Error ("unshift is not implemented for this type");
             else return substack.unshift(value);
         },
         'shift <|' (substack) {
             if (!(substack instanceof Polca.SubStack))
-            throw new Error ("pop is not implemented for this type");
+            throw new Error ("shift is not implemented for this type");
             else return substack.shift();
         },
 
