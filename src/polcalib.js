511af427842bf2b0d87141617463c8df5d50faaa
sofia <12581975+opensofias@users.noreply.github.com>
2022-01-05 01:43:12 +0100

    fixed: "dissolve" was called "integrate" in

--- src/polcalib.js
+++ src/polcalib.js
@@ -194,7 +194,7 @@ polcaLib = (function () {
         },
 
         dissolve: function (substack) {
-            if (!(substack instanceof Polca.SubStack)) throw new Error ("integrate is not implemented for this type");
+            if (!(substack instanceof Polca.SubStack)) throw new Error ("dissolve is not implemented for this type");
 
             return substack.ary;
         },
