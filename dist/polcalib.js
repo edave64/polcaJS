e0bd3dfcf3de9b5e618e0cb731af9eb7dcad7029
sofia <12581975+opensofias@users.noreply.github.com>
2022-01-09 20:39:59 +0100

    oops, set and get need "this"

--- dist/polcalib.js
+++ dist/polcalib.js
@@ -55,8 +55,8 @@ polcaLib = (function () {
                 throw '"!" only works on arrays and functions';
         },
         'typeof': (v) => v.type,
-        'set :': (value, name) => this.scope.set(name, value),
-        'get .': (name) => this.scope.get(name),
+        'set :'(value, name) { this.scope.set(name, value); },
+        'get .'(name) { return this.scope.get(name); },
         'setall ::'(value, names) {
             names.ary.forEach(name => this.scope.set(name, value));
         },
